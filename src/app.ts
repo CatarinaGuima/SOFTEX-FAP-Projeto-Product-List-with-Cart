7732550f1d9ce4bdad1a87eb2375337ba1be4c81
Catarina Guimarães <catarina.guimaraes15@gmail.com>
2024-09-18 11:56:57 -0300

    fourth commit

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
-import { Product } from './entities/product';
 import data from '../data.json';
+import { Product } from './entities/product';
 
-console.log(data);
-
-const products: string [] = [];
+for (const product of data) {
+     new Product(
+        product.name,
+        product.price,
+        product.category,
+        product.image.desktop
+    );
+}
